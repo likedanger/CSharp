8e041009e2209f248582e937d56df2b1ad31a2fb
ARang <32333049+likedanger@users.noreply.github.com>
2022-03-23 22:34:29 +0800

    Add files via upload

---
+++ Number_string/Number_string/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(string s)
+    {
+        Dictionary<string, string> result = new Dictionary<string, string>()
+        {
+            {"zero", "0" },
+            {"one", "1" },
+            {"two","2" },
+            {"three", "3" },
+            {"four", "4" },
+            {"five","5" },
+            {"six", "6" },
+            {"seven", "7" },
+            {"eight","8" },
+            {"nine", "9" },
+        };
+        foreach(var KeyValue in result)
+        {
+            s = s.Replace(KeyValue.Key, KeyValue.Value);
+        }
+
+            return Int32.Parse(s);
+        }
+    }
